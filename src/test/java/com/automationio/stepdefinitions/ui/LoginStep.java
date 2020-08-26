29b86b522f254d00b427b375db8e0255dbff8734
Anoop Simon <anoop.simon@adudev.com>
2020-08-26 15:57:39 +1000

    refactor

--- src/test/java/com/automationio/stepdefinitions/ui/LoginStep.java
+++ src/test/java/com/automationio/stepdefinitions/ui/LoginStep.java
@@ -2,7 +2,7 @@ package com.automationio.stepdefinitions.ui;
 
 import io.cucumber.java.en.Given;
 
-public class StepDefinitions {
+public class LoginStep {
 
 @Given("I launch {string} browser")
 public void i_launch_browser(String string) 
